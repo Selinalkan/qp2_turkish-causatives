70c1aae58d8e10982866d968262c2c27fec08dcf
Selinalkan <alkan.selin@gmail.com>
2024-02-28 00:36:01 -0500

    Adding sample files

---
+++ 4_Feb28/xml/LDC/xml-parser_ldc.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+# Open the XML file
+with open("short-LDC.xml", "r") as file:
+    data = []
+    in_text_tag = False
+    for line in file:
+        # Check if line contains the start of <text> tag
+        if "<text" in line:
+            in_text_tag = True
+            text_data = []
+        # Check if line contains the end of </text> tag
+        elif "</text>" in line:
+            in_text_tag = False
+            # Process the data collected between <text> and </text> tags
+            for text_line in text_data:
+                elements = text_line.strip().split()
+                if len(elements) == 5:  # Ensure there are exactly five elements
+                    data.append({
+                        'word': elements[0],
+                        'pos_tag': elements[1],
+                        'morph_parse': elements[2],
+                        'lemma': elements[3],
+                        'correct_form': elements[4]
+                    })
+        # Collect lines between <text> and </text> tags
+        elif in_text_tag:
+            text_data.append(line)
+
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Save DataFrame to TSV
+df.to_csv("output.tsv", sep='\t', index=False)
+
+print(df)
+
+
+
